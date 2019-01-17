4e124e994df67a0204054c9eb22a985859f41287
lisaQ04 <lquluba@retrorabbit.co.za>
2019-01-17 11:02:36 +0200

    area of shape calculated

---
+++ GitWorkshop/GitWorkshop/Pentagon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitWorkshop
+{
+    class Pentagon
+    {
+        double side = 0;
+
+        public void AreaPentagon()
+        {
+            double area_pentagon = 0;
+            area_pentagon = (1 / 4 * (Math.Sqrt(5 * (5 + 2 * (Math.Sqrt(5)))))) * (side * side); //must will and should always be zero.
+            Console.WriteLine("Enter the length of the side:");
+            side = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Area of Pentagon: " + area_pentagon);
+            Console.ReadKey();
+        }
+    }
+}
