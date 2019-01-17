b3ede614550e8b0744321ae6c59813be6f65e7ab
CastelloGovender <cgovender@retrorabbit.co.za>
2019-01-17 10:48:48 +0200

    Initial Circle class with area method

---
+++ GitWorkshop/GitWorkshop/Circle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitWorkshop
+{
+    class Circle
+    {
+        void areaC()
+        {
+            Console.WriteLine("Enter side length");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(3.14 * r * r);
+        }
+    }
+}
